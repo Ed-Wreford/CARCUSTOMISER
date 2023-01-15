b4b72331e8a6b159815e20e252ffd39d26b3c664
Ed-Wreford <wreford.e@etoncollege.org.uk>
2023-01-15 21:29:22 +0000

    Initial Commit

---
+++ CarCustomiser/Car.swift
@@ -0,0 +1,18 @@
+//
+//  Car.swift
+//  CarCustomiser
+//
+//  Created by Ed Wreford on 15/01/2023.
+//
+
+import Foundation
+
+struct Car {
+    
+    let make: String
+    let model: String
+    var topSpeed: Int
+    var acceleration: Double
+    var handling: Int
+    
+}
