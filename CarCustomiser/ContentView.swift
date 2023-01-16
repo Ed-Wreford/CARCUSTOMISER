1f7dff6683e20eb59ef0055d89c1e170decbc65e
Ed-Wreford <wreford.e@etoncollege.org.uk>
2023-01-16 14:37:02 +0000

    First Push

--- CarCustomiser/ContentView.swift
+++ CarCustomiser/ContentView.swift
@@ -13,16 +13,31 @@ struct ContentView: View {
     
     let car = Car(make: "Mazda", model: "MX-5", topSpeed: 125, acceleration: 7.7, handling: 5)
     
+    
     var body: some View {
-        VStack {
+        VStack(alignment: .leading) {
             Text("Make: \(car.make)")
+                    .foregroundColor(Color.red)
+            Text("Model: \(car.model)")
+                    .foregroundColor(Color.red)
+            Text("Top Speed: \(car.topSpeed)mph")
+                    .foregroundColor(Color.red)
+            Text("Acceleration (0-60): \(car.acceleration)s")
+                    .foregroundColor(Color.red)
+            Text("Handling: \(car.handling)")
+                    .foregroundColor(Color.red)
+                
+            }
+            .padding(1.0)
         }
-        .padding()
     }
-}
+    
+    
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            
     }
 }
--- CarCustomiserTests/CarCustomiserTests.swift
+++ CarCustomiserTests/CarCustomiserTests.swift
@@ -18,8 +18,25 @@ final class CarCustomiserTests: XCTestCase {
         XCTAssertEqual(car.make, "Mazda")
         XCTAssertEqual(car.model, "MX-5")
         XCTAssertEqual(car.topSpeed, 125)
-        XCTAssertEqual(car.acceleration, 7.7)
+        XCTAssertEqual(car.acceleration, 7.0)
         XCTAssertEqual(car.handling, 5)
     }
 
+    func testTheDisplayedStatsAreCorrect() {
+        //arrange
+        let car = Car(make: "Mazda", model: "MX-5", topSpeed: 125, acceleration: 7.7, handling: 5)
+        let expectedOutput = """
+Make: Mazda
+Model: MX-5
+Top Speed: 125mph
+Acceleration: 7.7s
+Handling: 5
+"""
+        //arrange
+        let actual = body
+        //assert
+        XCTAssertEqual(actual, expectedOutput)
+        
+        
+    }
 }
