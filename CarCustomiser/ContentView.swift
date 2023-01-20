f16129f03f683840103342acf8043d6a208aff66
Ed-Wreford <wreford.e@etoncollege.org.uk>
2023-01-20 23:01:58 +0000

    Part 2.6 with odd errors.

--- CarCustomiser/ContentView.swift
+++ CarCustomiser/ContentView.swift
@@ -11,7 +11,7 @@ import SwiftUI
 
 struct ContentView: View {
     
-    let starterCars = StarterCars()
+    @State private var starterCars = StarterCars()
     @State private var selectedCar: Int = 0 {
         
         didSet {
@@ -19,19 +19,86 @@ struct ContentView: View {
                 selectedCar  = 0
             }
         }
-        
     }
+    @State private var exhaustPackage = false
+    @State private var tiresPackage = false
+    @State private var nitroBoostPackage = false
+    @State private var godModePackage = false
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 20) {
-            Text("\(starterCars.cars[selectedCar].displayStats())")
-                .foregroundColor(Color.black)
-            
-            Button("Next Car", action: {
-                selectedCar += 1
+        
+        let exhaustPackageBinding = Binding<Bool> (
+            get: { self.exhaustPackageBinding }
+            set: { newValue in
+                self.exhaustPackage = newValue
+                if newValue == true {
+                    starterCars.cars[selectedCar].topSpeed += 5
+                } else {
+                    starterCars.cars[selectedCar].topSpeed -= 5
+                }
+            }
                 
-                //selectedCar = Int.random(in: 0 ..< self.starterCars.cars.count)
-            })
+        )
+        
+        let tiresPackageBinding = Binding<Bool> (
+            get: { self.tiresPackageBinding }
+            set: { newValue in
+                self.tiresPackage = newValue
+                if newValue == true {
+                    starterCars.cars[selectedCar].handling += 2
+                } else {
+                    starterCars.cars[selectedCar].handling -= 2
+                }
+            }
+                
+        )
+        
+        let nitroBoostPackageBinding = Binding<Bool> (
+            get: { self.nitroBoostPackageBinding }
+            set: { newValue in
+                self.nitroBoostPackage = newValue
+                if newValue == true {
+                    starterCars.cars[selectedCar].acceleration -= 2
+                } else {
+                    starterCars.cars[selectedCar].acceleration += 2
+                }
+            }
+                
+        )
+        
+        let godModePackageBinding = Binding<Bool> (
+            get: { self.godModePackageBinding }
+            set: { newValue in
+                self.godModePackage = newValue
+                if newValue == true {
+                    starterCars.cars[selectedCar].handling += 2
+                    starterCars.cars[selectedCar].acceleration -= 1
+                    starterCars.cars[selectedCar].topSpeed += 15
+                } else {
+                    starterCars.cars[selectedCar].handling -= 2
+                    starterCars.cars[selectedCar].acceleration += 1
+                    starterCars.cars[selectedCar].topSpeed += 15
+                }
+            }
+                
+        )
+        
+        Form {
+            VStack(alignment: .leading, spacing: 20) {
+                Text("\(starterCars.cars[selectedCar].displayStats())")
+                    .foregroundColor(Color.black)
+                
+                Button("Next Car", action: {
+                    selectedCar += 1
+                    
+                })
+            }
+            Section {
+                Toggle("Exhaust package (Cost: 500)", isOn: exhaustPackageBinding)
+                Toggle("Tires package (Cost: 500)", isOn: tiresPackageBinding)
+                Toggle("Nitro boost package (Cost: 500)", isOn: nitroBoostPackageBinding)
+                Toggle("God package (Cost: 1000)", isOn: godModePackageBinding)
+            }
         }
     }
     
--- CarCustomiser/StarterCars.swift
+++ CarCustomiser/StarterCars.swift
@@ -10,6 +10,6 @@ import Foundation
 struct StarterCars {
 
     
-    let cars = [Car(make: "Mazda", model: "MX-5", topSpeed: 125, acceleration: 7.7, handling: 5), Car(make: "Ford", model: "Focus", topSpeed: 110, acceleration: 4.2, handling: 3), Car(make: "Lotus", model: "Emira", topSpeed: 140, acceleration: 8.9, handling: 7), Car(make: "Toyota", model: "Corolla", topSpeed: 115, acceleration: 4, handling: 6), Car(make: "Volkswagen", model: "Golf", topSpeed: 105, acceleration: 5.3, handling: 3)]
+    var cars = [Car(make: "Mazda", model: "MX-5", topSpeed: 125, acceleration: 7.7, handling: 5), Car(make: "Ford", model: "Focus", topSpeed: 110, acceleration: 4.2, handling: 3), Car(make: "Lotus", model: "Emira", topSpeed: 140, acceleration: 8.9, handling: 7), Car(make: "Toyota", model: "Corolla", topSpeed: 115, acceleration: 4, handling: 6), Car(make: "Volkswagen", model: "Golf", topSpeed: 105, acceleration: 5.3, handling: 3)]
     
 }
