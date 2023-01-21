be3a8c6d8c5be46cdfaec71cd1ce8da00364b7e6
Ed-Wreford <wreford.e@etoncollege.org.uk>
2023-01-21 21:13:00 +0000

    nearly all part 2 done, need to do reset toggle function and fixed newValue error

--- CarCustomiser/ContentView.swift
+++ CarCustomiser/ContentView.swift
@@ -24,6 +24,43 @@ struct ContentView: View {
     @State private var tiresPackage = false
     @State private var nitroBoostPackage = false
     @State private var godModePackage = false
+    @State private var remainingFunds = 1000
+    
+    var exhaustPackageEnabled: Bool {
+        if remainingFunds - 500 >= 0 {
+            return true
+        }
+        else {
+            return false
+        }
+    }
+    
+    var tiresPackageEnabled: Bool {
+        if remainingFunds - 500 >= 0 {
+            return true
+        }
+        else {
+            return false
+        }
+    }
+    
+    var nitroBoostPackageEnabled: Bool {
+        if remainingFunds - 500 >= 0 {
+            return true
+        }
+        else {
+            return false
+        }
+    }
+    
+    var godModePackageEnabled: Bool {
+        if remainingFunds - 1000 >= 0 {
+            return true
+        }
+        else {
+            return false
+        }
+    }
     
     var body: some View {
         
@@ -33,11 +70,13 @@ struct ContentView: View {
                 self.exhaustPackage = newValue
                 if newValue == true {
                     starterCars.cars[selectedCar].topSpeed += 5
+                    remainingFunds -= 500
                 } else {
                     starterCars.cars[selectedCar].topSpeed -= 5
+                    remainingFunds  += 500
                 }
             }
-                
+            
         )
         
         let tiresPackageBinding = Binding<Bool> (
@@ -46,11 +85,13 @@ struct ContentView: View {
                 self.tiresPackage = newValue
                 if newValue == true {
                     starterCars.cars[selectedCar].handling += 2
+                    remainingFunds -= 500
                 } else {
                     starterCars.cars[selectedCar].handling -= 2
+                    remainingFunds  += 500
                 }
             }
-                
+            
         )
         
         let nitroBoostPackageBinding = Binding<Bool> (
@@ -59,11 +100,13 @@ struct ContentView: View {
                 self.nitroBoostPackage = newValue
                 if newValue == true {
                     starterCars.cars[selectedCar].acceleration -= 2
+                    remainingFunds -= 500
                 } else {
                     starterCars.cars[selectedCar].acceleration += 2
+                    remainingFunds  += 500
                 }
             }
-                
+            
         )
         
         let godModePackageBinding = Binding<Bool> (
@@ -74,33 +117,52 @@ struct ContentView: View {
                     starterCars.cars[selectedCar].handling += 2
                     starterCars.cars[selectedCar].acceleration -= 1
                     starterCars.cars[selectedCar].topSpeed += 15
+                    remainingFunds -= 1000
                 } else {
                     starterCars.cars[selectedCar].handling -= 2
                     starterCars.cars[selectedCar].acceleration += 1
                     starterCars.cars[selectedCar].topSpeed += 15
+                    remainingFunds  += 1000
                 }
             }
-                
+            
         )
-        
-        Form {
-            VStack(alignment: .leading, spacing: 20) {
-                Text("\(starterCars.cars[selectedCar].displayStats())")
-                    .foregroundColor(Color.black)
-                
-                Button("Next Car", action: {
-                    selectedCar += 1
+        VStack {
+            Form {
+                VStack(alignment: .leading, spacing: 20) {
+                    Text("\(starterCars.cars[selectedCar].displayStats())")
+                        .foregroundColor(Color.black)
                     
-                })
+                    Button("Next Car", action: {
+                        selectedCar += 1
+                        resetDisplay()
+                    })
+                }
+                Section {
+                    Toggle("Exhaust package (Cost: 500)", isOn: exhaustPackageBinding)
+                        .disabled(!exhaustPackageEnabled)
+                    Toggle("Tires package (Cost: 500)", isOn: tiresPackageBinding)
+                        .disabled(!tiresPackageEnabled)
+                    Toggle("Nitro boost package (Cost: 500)", isOn: nitroBoostPackageBinding)
+                        .disabled(!nitroBoostPackageEnabled)
+
+                    Toggle("God package (Cost: 1000)", isOn: godModePackageBinding)
+                        .disabled(!godModePackageEnabled)
+
+                }
             }
-            Section {
-                Toggle("Exhaust package (Cost: 500)", isOn: exhaustPackageBinding)
-                Toggle("Tires package (Cost: 500)", isOn: tiresPackageBinding)
-                Toggle("Nitro boost package (Cost: 500)", isOn: nitroBoostPackageBinding)
-                Toggle("God package (Cost: 1000)", isOn: godModePackageBinding)
-            }
+            Text("Remaining Funds: \(remainingFunds)")
+                .foregroundColor(.red)
+                .baselineOffset(20)
         }
+        
+     func resetDisplay()
+        
+        
+        
     }
+    
+    
     
     
     
